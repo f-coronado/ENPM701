45001823a5005a16de2163dc97241f6aed3586ee
Fabrizzio <fabrizzio37@gmail.com>
2024-03-24 02:33:40 -0400

    servo/servocontrol01.py is now working, also reorganized folders

--- servo/servocontrol01.py
+++ servo/servocontrol01.py
@@ -2,6 +2,28 @@ import RPi.GPIO as gpio
 import time
 import cv2 as cv
 
+# setup gpio pins and set pin 36 as output 
+gpio.setmode(gpio.BOARD)
+gpio.setup(36, gpio.OUT)
+pwm = gpio.PWM(36, 50)
+pwm.start(5) # start PWM frequency to 5% duty cycle
+
+# initialize camera
+cap = cv.VideoCapture(0)
+if not cap.isOpened():
+	print("couldn't open camera")
+	exit()
+
+# define video properties
+video_file = "dutyCycle.mp4"
+fps = 1
+fourcc = cv.VideoWriter_fourcc(*'XVID')
+out = cv.VideoWriter('dutyCycle.mp4', fourcc, fps, (int(cap.get(3)), int(cap.get(4))))
+
+min_cycle = 3
+max_cycle = 9
+
+
 def cycle(duration):
 
 	pwm.ChangeDutyCycle(4)
@@ -27,7 +49,7 @@ def set_cycle(duty_cycle):
 def write_on_frame(duty_cycle):
 
 	font = cv.FONT_HERSHEY_SIMPLEX
-	location = (15, 15)
+	location = (15, 25)
 	white = (255, 255, 255)
 	thickness = 2
 	scale = 1
@@ -36,33 +58,17 @@ def write_on_frame(duty_cycle):
 	cv.putText(frame, "Duty: {}%".format(duty_cycle), location, font, scale, white, thickness) 
 	cv.imwrite("duty_{}.jpg".format(duty_cycle), frame)
 	
+#	return frame
+
 def create_video():
+	
 	for i in range(min_cycle, max_cycle + 1):
 		frame = cv.imread("duty_{}.jpg".format(i))
 		out.write(frame)
+	
+#	return out
 
 def main(duration):
-
-	# setup gpio pins and set pin 36 as output 
-	gpio.setmode(gpio.BOARD)
-	gpio.setup(36, gpio.OUT)
-	pwm = gpio.PWM(36, 50)
-	pwm.start(5) # start PWM frequency to 5% duty cycle
-
-	# initialize camera
-	cap = cv.VideoCapture(0)
-	if not cap.isOpened():
-		print("couldn't open camera")
-		exit()
-
-	# define video properties
-	video_file = "dutyCycle.mp4"
-	fps = 30
-	fourcc = cv.VideoWriter_fourcc(*'XVID')
-	out = cv.VideoWriter('dutyCycle.mp4', fourcc, fps, (int(cap.get(3)), int(cap.get(4))))
-
-	min_cycle = 3
-	max_cycle = 9
 
 	pwm.start(min_cycle)
 	
@@ -74,6 +80,12 @@ def main(duration):
 		set_cycle(duty_cycle)
 		time.sleep(duration)
 		write_on_frame(duty_cycle)
+
+#		if duty_cycle == min_cycle:
+#			gripper_closed = True
+#			break
+
+	
 
 	create_video()
 	pwm.stop()
