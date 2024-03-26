428dec5d77b03419a9290665f12b72f78345c052
Fabrizzio <fabrizzio37@gmail.com>
2024-03-26 18:24:38 -0400

    need to test encodercontrol02

---
+++ motor/encodercontrol02.py
@@ -0,0 +1,67 @@
+import RPi.GPIO as gpio
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+## initialize gpio pins ##
+
+def init():
+	gpio.setmode(gpio.BOARD)
+	gpio.setup(31, gpio.OUT) # IN1
+	gpio.setup(33, gpio.OUT) # IN2
+	gpio.setup(35, gpio.OUT) # IN3
+	gpio.setup(37, gpio.OUT) # IN4
+
+	gpio.setup(12, gpio.IN, pull_up_down = gpio.PUD_UP)
+
+def gameover():
+	
+	gpio.output(31, False)
+	gpio.output(33, False)
+	gpio.output(35, False)
+	gpio.output(37, False)
+
+	gpio.cleanup()
+
+## main code ##
+init()
+
+counter = np.uint64(0)
+button = int(0)
+
+# initialize pwm signal to control motor
+pwm = gpio.PWM(37, 50)
+val = 14
+pwm.start(val)
+time.sleep(0.1)
+
+x_vals = []
+y_vals = []
+
+with open('encodercontrol02.txt', 'w') as file: # open this file in write mode
+	file.write("Counter\t GPIO state\n") 
+	
+	for i in range(0, 100000):
+		file.write(f"{counter}\t{gpio.input(12)}\n") # write to the file
+
+		x_vals.append(gpio.input(12))
+		y_vals.append(counter)
+
+		print("counter = ", counter, "GPIO state: ", gpio.input(12))
+
+		if int(gpio.input(12)) != int(button):
+			button = int(gpio.input(12))
+			counter += 1
+
+		if counter >= 960:
+			pwm.stop()
+			gameover()
+			print("thanks for playing")
+			break
+
+	
+plt.plot(x_vals, y_vals)
+plt.xlabel('GPIO input reading')
+plt.ylabel('Encoder State')
+plt.title('Motor Encoder Analysis')
+plt.show()
