c541332aefc3407cc026270cad26085eb53cf989
Fabrizzio <fabrizzio37@gmail.com>
2024-04-05 21:55:07 -0400

    added encodercontrol05.py

--- motor/encodercontrol05.py
+++ motor/encodercontrol05.py
@@ -25,15 +25,18 @@ def gameover():
 
 	gpio.cleanup()
 
-def right_wheels():
+def get_input():
 
-	init()
+	distance = float(input("enter how long you want the robot to drive in meters(<1m):"))
 
-	gpio.output(35, False)
-	gpio.output(37, True)
-	time.sleep(2)
+	return round(distance, 4)
 
-	#gameover()
+def get_tick_cnt(dist):
+
+	ticks = (dist * 120 * 8)/.2048
+
+	return ticks 
+
 
 ## main code ##
 init()
@@ -42,6 +45,12 @@ counterBR = np.uint64(0)
 
 buttonFL = int(0)
 buttonBR = int(0)
+
+# hw7
+travel_distance = get_input()
+print("distance to travel is: ", travel_distance)
+travel_ticks = get_tick_cnt(travel_distance)
+print(travel_ticks, "ticks are needed to travel ", travel_distance, "m")
 
 # independent motor control via pwm
 pwmBL = gpio.PWM(31, 50)
@@ -60,7 +69,7 @@ with open('encodercontrol04.txt', 'w') as file: # open this file in write mode
 	file.write("BRcnt  BR GPIO  FLcnt  FLGPIO\n") 
 
 	for i in range(0, 100000):
-		print("counterBR: ", counterBR, "counterFL: ", counterFL, "BR state: ", gpio.input(12), "FL state: ", gpio.input(7))
+#		print("counterBR: ", counterBR, "counterFL: ", counterFL, "BR state: ", gpio.input(12), "FL state: ", gpio.input(7))
 		file.write(f"{counterBR}\t{gpio.input(12)}\t{counterFL}\t{gpio.input(7)}\n") # write to the file
 
 		if int(gpio.input(12)) != int(buttonBR):
@@ -75,13 +84,13 @@ with open('encodercontrol04.txt', 'w') as file: # open this file in write mode
 			counterFL += 1
 			FL_counter.append(counterFL)
 
-		if counterFL >= 960:
+		if counterFL >= travel_ticks:
 			pwmBL.stop()
 
-		if counterBR >= 960:
+		if counterBR >= travel_ticks:
 			pwmFR.stop()
 
-		if counterBR >= 960 and counterFL >= 960:
+		if counterBR >= travel_ticks and counterFL >= travel_ticks:
 			gameover()
 			print("thanks for playing")
 			break
